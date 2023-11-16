3ff5ba2f8fc394adc144e2852b0f0f87561d72ce
Madavi1234 <129905460+Madavi1234@users.noreply.github.com>
2023-11-16 15:19:26 +0530

    Add files via upload

---
+++ list using while.py
@@ -0,0 +1,14 @@
+list = [2, 4, 6, 8, 10]
+
+# Getting length of list
+
+length = len(list)
+print("length is ",length)
+
+i = 0
+
+# Iterating using while loop
+
+while i < length:
+    print(list[i])
+    i += 1
